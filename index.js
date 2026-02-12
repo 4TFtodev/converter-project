77fd47342fbf0231574dffd3b51eee6b4d153ac8
4TFtodev <lucas.hqs91@gmail.com>
2026-02-12 01:21:40 +0100

    Add unit converter project files

---
+++ index.js
@@ -0,0 +1,9 @@
+let inputEl = document.getElementById("inputEl");
+let convertBtn = document.getElementById("convertBtn");
+let lengthResult = document.getElementById("lengthResult");
+let volumeResult = document.getElementById("volumeResult");
+let massResult = document.getElementById("massResult");
+
+converterBtn.addEventListener('click', function() {
+    
+})
